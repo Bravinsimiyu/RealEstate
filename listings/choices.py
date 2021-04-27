64c38a2bc96eed3d0dbc9c52453c22402169d36c
Bravinsimiyu <bravinwasike18@gmail.com>
2021-04-27 14:08:08 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ listings/choices.py
@@ -0,0 +1,80 @@
+bedroom_choices = {
+    '1':1,
+    '2':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10
+   
+}
+
+price_choices = {
+    '100000':'ksh100,000',
+    '200000':'ksh200,000', 
+    '300000':'ksh300,000', 
+    '400000':'ksh400,000', 
+    '500000':'ksh500,000', 
+    '600000':'ksh600,000', 
+    '700000':'ksh700,000', 
+    '800000':'ksh800,000', 
+    '900000':'ksh900,000', 
+    '1000000':'ksh1M+',     
+                    
+}
+
+state_choices = {
+    'MB':'Mombasa',
+    'KW':'Kwale',
+    'KL':'Kilifi',
+    'TN':'Tana River',
+    'LM':'Lamu',
+    'TT':'Taita-Taveta',
+    'GS':'Garissa',
+    'WJ':'Wijir',
+    'MD':'Mandera',
+    'MR':'Marsabit',
+    'IS':'Isiolo',
+    'MU':'Meru',
+    'TN':'Tharaka-Nithi',
+    'EM':'Embu',
+    'KT':'Kitui',
+    'MC':'Machakos',
+    'MK':'Makueni',
+    'ND':'Nyandarua',
+    'NY':'Nyeri',
+    'KR':'Kirinyaga',
+    'MR':'Muranga',
+    'KU':'Kiambu',
+    'TR':'Turkana',
+    'WP':'West Pokot',
+    'SM':'Samburu',
+    'TN':'Trans-Nzoia',
+    'UG':'Uasin Gashu',
+    'EM':'Elgeyo-Marakwet',
+    'NA':'Nandi',
+    'BR':'Baringo',
+    'LK':'Laikipia',
+    'NK':'Nakuru',
+    'NA':'Narok',
+    'KJ':'Kajiado',
+    'KE':'Kericho',
+    'BO':'Bomet',
+    'KK':'Kakamega',
+    'VI':'Vihiga',
+    'BG':'Bungoma',
+    'BU':'Busia',
+    'SI':'Siaya',
+    'KI':'Kisumu',
+    'HB':'Homa Bay',
+    'MI':'Migori',
+    'KI':'Kisi',
+    'NY':'Nyamira',
+    'NA':'Nairobi'
+   
+                     
+                     
+}
+                   
